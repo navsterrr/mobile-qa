8e7daa9f0a48470defbc98fc7989629e6c813ba1
Natalie Kramer <natalie.kramer@teamsnap.com>
2024-04-09 14:15:54 -0500

    fix: remove test case

--- src/test/java/com/teamsnap/AppTest.java
+++ src/test/java/com/teamsnap/AppTest.java
@@ -3,19 +3,10 @@ package com.teamsnap;
 import config.BrowserStackConfig;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import pages.LoginPage;
-
 
 public class AppTest extends BrowserStackConfig {
 
     private final SoftAssert assertion = new SoftAssert();
 
-    @Test(description = "Android - Click on login button")
-    public void clickLoginButtonTest() {
-        LoginPage loginPage = new LoginPage(appiumDriver);
-
-        loginPage
-                .clickLogin();
-    }
+    /* Write your test steps here */
 }
-
